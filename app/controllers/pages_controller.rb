5376ff0e8e24fd86da4db9190a59da16002b0ab1
pottermichael <potter.michael@gmail.com>
2019-02-04 20:21:06 -0600

    tried to add app and config

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def vision
+  end
+
+  def facility
+  end
+
+  def amenities
+  end
+
+  def transit
+  end
+
+  def transitway
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+
+  get 'vision', to: 'pages#vision'
+
+  get 'facility', to: 'pages#facility'
+
+  get 'amenities', to: 'pages#amenities'
+
+  get 'transit', to: 'pages#transit'
+
+  get 'transitway', to: 'pages#transitway'
+
+end
