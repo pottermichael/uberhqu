272fc41fd15c9728599890436e0736172d481bb5
pottermichael <potter.michael@gmail.com>
2019-02-05 22:41:04 -0600

    substitute img for aws s3, fixed navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
 //= require rails-ujs
+//= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require jquery3
